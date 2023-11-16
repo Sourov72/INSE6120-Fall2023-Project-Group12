f258f94e534b71029f64e3fe994d481cdce5d049
Piyush Adhikari <adhikaripiyush41@gmail.com>
2023-11-16 13:07:02 -0500

    Merged poodle branch to main branch.

---
+++ PoodleImplementation/poodle-dev.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ -z $1 ]
+then
+    cat <<HELPMSG
+What do you need to start?
+- httpserver: target http server
+- sslserver: sslv3 forwarder to httpd
+- attacker: POODLE attack script configured for localhost
+HELPMSG
+elif [ "$1" == "httpserver" ]
+then
+    ./TestHTTPServer.py
+elif [ "$1" == "sslserver" ]
+then
+    socat -v -x OPENSSL-LISTEN:4433,verify=0,method=SSLv3,cert=cert-poodle.pem,key=key-poodle.pem,reuseaddr,fork TCP:localhost:4080
+elif [ "$1" == "attacker-debug" ]
+then
+    ./poodle.py -d --start-offset 384 --target-port 4433 https://localhost:8443
+elif [ "$1" == "attacker" ]
+then
+    ./poodle.py --start-offset 384 --target-port 4433 https://localhost:8443
+fi
